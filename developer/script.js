7612d34ba30245c3a4c28cf99241a5948f788cd1
ahmedisti <mdistiaqah@gmail.com>
2022-01-13 12:28:38 +0600

    coding challenge done

--- developer/script.js
+++ developer/script.js
@@ -16,7 +16,7 @@
 //find max value in temp array
 // find min value in temp array
 // subtract min from max(amplitude) and return it
-
+/*
 const temperatures1 = [3, -2, -6, "error", 9];
 const temperatures2 = [13, 17, 15, 14, 9, 5];
 
@@ -41,7 +41,7 @@ const tempAmplitude = calcTempAmplitude(temperatures1, temperatures2);
 // max = 3
 
 console.log(tempAmplitude);
-
+*/
 //problem 2
 
 //function now should receive two arrays
@@ -72,3 +72,55 @@ console.log(tempAmplitude);
 
 
 // console.log(measureKelvin());
+
+
+/*
+Coding Challenge #1
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+
+*/
+
+//solution
+
+//===Understand the problem===//
+// - array transformed into string , separeted by ...
+// -what is the x days? curret index+1
+
+
+//===Break into sub-problem===//
+//-transform the array into string
+// - transform element to string to °c
+// string need to contain the days = current-index[] + 1
+// add ... between element and start and end of string
+
+/*
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+
+const printForecast = function(arr){
+
+  let str = '';
+
+  for(let i = 0; i < arr.length; i++){
+
+    str += `${arr[i]}°C in ${i + 1} days ... `
+  }
+  console.log('... ' + str);
+};
+
+printForecast(data2);
+
+*/
+
+// A lot of hardwork needed;
