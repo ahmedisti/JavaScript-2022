f379b1bb4602f50be6921f860c03f8fbdee97dcc
ahmedisti <mdistiaqah@gmail.com>
2022-02-02 16:38:15 +0600

    string final practice and Data Structures and Modern Operators done

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
 
 // Data needed for first part of the section
 
@@ -997,7 +995,7 @@ name conversion working 😉
 */
 
 // Solution:
-
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -1018,4 +1016,29 @@ document.querySelector('button').addEventListener('click',function(){
       )}`;
     console.log(`${output.padEnd(20)}${'✅'.repeat(i+1)}`);
   }
-})
+});
+
+*/
+
+// String Method Practice 
+
+// Data needed for  exercise
+
+/*
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+  const getCode = str => {
+    return str.slice(0,3).toUpperCase();
+  }
+
+  for(const flight of flights.split('+')){
+
+    const[type,from,to,time] = flight.split(';');
+
+    const output = `${type.startsWith('_Delayed')? '🔴': ' '}${type.replaceAll('_','')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':','h')})`.padStart(45);
+    console.log(output);
+  }
+
+  */
+
