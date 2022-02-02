333fa600baba48401d36c45498fd8d6147020205
ahmedisti <mdistiaqah@gmail.com>
2022-02-02 17:40:21 +0600

    function start and part 1 default function

---
+++ 10-Functions/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+//============= Default Parameters =============\\
+
+/*
+const bookings = [];
+
+const createBooking = function(
+    flightNum, 
+    numPassenger = 1,
+    price = 199 * numPassenger
+    ){ // ES6 to set default values
+
+
+
+    //ES5 way to set default value
+    // numPassenger = numPassenger || 1;
+    // price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassenger,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+
+}
+
+createBooking('LH123');
+createBooking('LH123',20,2000);
+createBooking('AB12',45)
+createBooking('BC234',undefined,1000); // when we don't want to pass the numPassenger arguments and set it to default value
+
+*/
