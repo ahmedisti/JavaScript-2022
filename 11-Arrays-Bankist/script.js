57f53a5ec37454bbf744ac49c0f1bebc9d01e7b3
ahmedisti <mdistiaqah@gmail.com>
2022-02-07 18:47:06 +0600

    coding challenge 3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -444,3 +444,33 @@ calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 //   .map(mov => mov * eurToUsd)
 //   .reduce((acc, mov) => acc + mov, 0);
 // console.log(totalDepositeUSD);
+
+// ========== CODING CHALLENGE 3 ========== \\
+//QUESTION
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+*/
+
+//SOLUTION
+
+// const calcAverageHumanAge = age =>{
+
+//   const average = age
+//   .map(age => age <= 2  ? 2 * age : 16 + (age * 4))
+//   .filter(age => age >= 18)
+//   .reduce((acc,current,i,arr) => (acc += current / arr.length)
+//    ,0);
+//    return average;
+// }
+// const age1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const age2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+// console.log(age1,age2);
+
+
+
