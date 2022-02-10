c2d7c9d58d61b8629183b42d0c0ee00e60b0570e
ahmedisti <mdistiaqah@gmail.com>
2022-02-10 16:32:30 +0600

    date format update

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-01-31T17:01:17.194Z',
+    '2022-02-07T18:49:59.371Z',
+    '2022-02-09T12:01:20.894Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -42,8 +42,8 @@ const account2 = {
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-01-T18:49:59.371Z',
+    '2022-02-07T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,21 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formateMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -90,13 +105,10 @@ const displayMovements = function (acc, sort = false) {
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const date = new Date(acc.movementsDates[i]);
-
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
-
+    const displayDate = formateMovementDate(date);
+    // `${day}/${month}/${year}`;
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -414,3 +426,12 @@ btnSort.addEventListener('click', function (e) {
 // console.log(future.getMinutes());
 // console.log(future.getSeconds());
 // console.log(future.toISOString());
+
+//==========Operation with Dates==========\\
+const future = new Date(2025, 1, 10, 2, 56);
+console.log(+future);
+
+// const calcDaysPassed = (date1,date2) => Math.abs(date2 - date1)/(1000*60*60*24);
+
+// const days =  calcDaysPassed(new Date(2037,3,14), new Date(2037,3,24));
+// console.log(days);
