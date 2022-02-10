58049a908dbc63c2a31cc204b098f6a7c1311b97
ahmedisti <mdistiaqah@gmail.com>
2022-02-10 13:01:28 +0600

    Math .

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -254,32 +254,73 @@ btnSort.addEventListener('click', function (e) {
 
 //==========The Numbers==========\\
 
-console.log(23 === 23.0);
-// base 10 --> 0 to 9
-// binary base 2 --> 0 & 1
+// console.log(23 === 23.0);
+// // base 10 --> 0 to 9
+// // binary base 2 --> 0 & 1
 
-console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2);
 
-//concersion
-console.log(Number('23'));
-console.log(+'23');
-// Can replace the Number to + operant and will give the same result
+// //concersion
+// console.log(Number('23'));
+// console.log(+'23');
+// // Can replace the Number to + operant and will give the same result
 
-//parsing
+// //parsing
 
-//check if the value is NaN
-console.log(Number('30px'));
-console.log(Number.parseInt('30px',10));
-console.log(Number.parseInt('e30',10));
-console.log(parseFloat('2.5rem'));
-console.log(Number.isNaN(20));
-console.log(Number.isNaN(+'20X'));
-console.log(Number.isNaN(23 / 0));
+// //check if the value is NaN
+// console.log(Number('30px'));
+// console.log(Number.parseInt('30px',10));
+// console.log(Number.parseInt('e30',10));
+// console.log(parseFloat('2.5rem'));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
 
-// check if a value is number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20X'));
-console.log(Number.isFinite(23 / 0));
+// // check if a value is number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
 
-console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23));
+
+
+//==========Math & Rounding==========\\
+// //squre Root
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1/2)); // here (1/2) is the square root
+// console.log(8 ** (1/3)); // here (1/2) is the qubick root
+// console.log(Math.max(1,2,3,4,5,6,7));
+// console.log(Math.max(1,2,3,'4',5,6,7));
+// console.log(Math.max(1,2,3,'4px',5,6,7));
+// console.log(Math.min(1,2,3,4,5,6,7));
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6)+1);
+
+// const randomInt = (min,max) => Math.trunc(Math.random() * (max-min) + 1) + min;
+// console.log(randomInt(10,20));
+
+// //ROUNDING INTEGER
+// console.log('---------------trunc--------------');
+// console.log(Math.trunc(23.3));
+// console.loground
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+// console.logceil
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+// console.logfloor
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+// console.log('-----------------------------');
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+
+// //rounding decimal
+// console.log('--------------toFixed---------------');
+// console.log((2.7).toFixed(0)); //return string
+// console.log((2.7).toFixed(3));
+// console.log(+(2.7234).toFixed(3));
+
