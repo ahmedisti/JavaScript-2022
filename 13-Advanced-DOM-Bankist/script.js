c2e148a745461c932ed09f82901fcd61619fa2bd
ahmedisti <mdistiaqah@gmail.com>
2022-02-15 15:11:44 +0600

    DOM Traversing

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -235,3 +235,42 @@ document.querySelector('.nav__links').addEventListener('click',function(e){
 //   console.log('Nav Link', e.target, e.currentTarget);
 //   this.style.backgroundColor = randomColor();
 // });
+
+//======== DOM Traversing ========\\
+
+// const h1 =  document.querySelector('h1');
+
+// //going downward: child
+
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+
+// console.log(h1.firstElementChild.style.color = 'white');
+// console.log(h1.lastElementChild.style.color = 'white');
+
+
+// //going upwards: parents
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// console.log(h1.closest('.header'));
+
+
+// //sideways : siblings
+
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+
+// [...h1.parentElement.children].forEach(function(el){
+//   if(el !== h1) el.style.transform = 'scale(0.5)';
+// });
