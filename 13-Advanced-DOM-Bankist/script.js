b65b2f34f553e8fb7a334ff88c51c1a7599cf2f9
ahmedisti <mdistiaqah@gmail.com>
2022-02-15 17:32:21 +0600

    sticky nav:intersection Observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -171,7 +171,36 @@ nav.addEventListener('mouseout',handleHoverFade.bind(1))
 
 //stciky navigation: intersection Observer API
 //best way
+// const observerCallback = function(entries,observer){
+// entries.forEach(entry => {
+//   console.log(entry);
+// })
+// }
+// const observerOption = {
+//   root: null,
+//   threshold:[0,0.2],
+// };
+// const observer =  new IntersectionObserver(observerCallback,observerOption);
+// observer.observe(section1)
 
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav =  function(entries){
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky')
+  
+}
+
+const headerObserver = new IntersectionObserver(stickyNav,{
+  root:null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header)
 
 ///////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
