6149bf3db42bbfd505992210175c34568956d82d
ahmedisti <mdistiaqah@gmail.com>
2022-02-17 16:10:10 +0600

    constructor function and new operator

---
+++ 14-OOP/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+//======= constructor function and new operator =======\\
+/*
+const Person = function(firstName,birthYear){
+
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    //Never do this inside of a constructor function
+    // this.calcAge =  function(){
+    //     console.log(2037 - this.birthYear);
+    // }
+
+}
+
+const istiaq = new Person('Istiaq',1998);
+console.log(istiaq);
+console.log(istiaq instanceof Person);
+// 1. New {} is created
+// 2. function is call , this = {} empty object
+// 3. link to prototype
+// 4. function auto return {}
+
+const ahmed  = new Person('Ahmed',1994);
+
+console.log(ahmed);
+
+*/
