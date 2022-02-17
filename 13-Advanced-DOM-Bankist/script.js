e97fb13f328e1e2e73d4af01590c5e80d2900bb4
ahmedisti <mdistiaqah@gmail.com>
2022-02-17 11:58:53 +0600

    Life cycle DOM event DOMContentLoaded, load, beforeunload

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -530,3 +530,20 @@ slider();
 // [...h1.parentElement.children].forEach(function(el){
 //   if(el !== h1) el.style.transform = 'scale(0.5)';
 // });
+
+
+//======= LIFECYCLE DOM EVENTS ========\\
+
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('hello',e);
+});
+
+window.addEventListener('load',function(e){
+  console.log('page loaded',e);
+});
+
+// window.addEventListener('beforeunload',function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
