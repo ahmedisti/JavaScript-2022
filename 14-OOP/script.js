4e9caf256ea2ec8c6446d7fe2f9169f2920caaef
ahmedisti <mdistiaqah@gmail.com>
2022-02-23 16:25:50 +0600

    ES6 classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -189,3 +189,43 @@ ford.accelerate();
 ford.break();
 ford.speedUS = 50;
 console.log(ford);
+
+
+// ES6 classes summery
+
+class Student extends Person{
+    university = 'SEU';
+    #studyHour = 0;
+    #course;
+    static numSubjects = 10;
+    constructor(fullName,birthYear,startYear,course){
+        super(fullName,birthYear);
+        this.startYear = startYear;
+        this.#course = course;
+    }
+
+    introduce(){
+        console.log(`I study ${this.course} at ${this.university}`);
+    }
+    study(h){
+        this.#makeCoffe();
+        this.#studyHour += h;
+    }
+    #makeCoffe(){
+        return `here is a coffe for you`
+    }
+
+    get testScore(){
+        return this._testScore;
+    }
+    set testScore(score){
+
+        this._testScore = score < 20 ? score : 0;
+
+    }
+    static printCurriculum(){
+        console.log(`There are ${this.numSubjects} subjects`);
+    }
+}
+
+const student = new Student('Istiaq',2020,2037,'CSE');
